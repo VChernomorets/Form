b16ac352a62026d8c428d24c719dcae0d2738c6c
VChernomorets <48764483+VChernomorets@users.noreply.github.com>
2019-06-17 15:26:39 +0300

    PS2 v3

--- js/main.js
+++ js/main.js
@@ -4,12 +4,15 @@ let passwordFlag = false;
 let usernameFlag = false;
 let locationFlag = false;
 // input email
-let email = document.getElementById('email');
+const email = document.getElementById('email');
+// minimum password size
+const minPassSize = 8;
+
 /**
  * Verification of the form to enter email
  */
  checkEmail = () => {
-    let regexp = new RegExp("^([a-z0-9_-]+\\.)*[a-z0-9_-]+@[a-z0-9_-]+(\\.[a-z0-9_-]+)*\\.[a-z]{2,6}$");
+    const regexp = new RegExp("^([a-z0-9_-]+\\.)*[a-z0-9_-]+@[a-z0-9_-]+(\\.[a-z0-9_-]+)*\\.[a-z]{2,6}$");
     if(!regexp.test(email.value)){
         email.classList.add("error");
         emailFlag = false;
@@ -37,11 +40,11 @@ password = document.getElementById('password');
  * If the characters are less than 8, underline the field.
  */
 checkPassword = () => {
-    if(password.value.length < 8){
-        email.classList.add("error");
+    if(password.value.length < minPassSize){
+        password.classList.add("error");
         passwordFlag = false;
     } else {
-        email.classList.remove("error");
+        password.classList.remove("error");
         passwordFlag = true;
     }
 };
@@ -57,7 +60,7 @@ firstCheckPassword = () => {
 // Create event 'blur'
 password.addEventListener('blur', firstCheckPassword);
 // Button save registration form
-let registrationSave = document.getElementById('registrationSave');
+const registrationSave = document.getElementById('registrationSave');
 /**
  * After pressing the button, the forms are checked.
  * If everything is filled out correctly, it shows the new form, and hides the old one.
@@ -65,8 +68,8 @@ let registrationSave = document.getElementById('registrationSave');
  */
 registrationSave.addEventListener('click', () => {
    if(emailFlag && passwordFlag){
-       document.getElementsByClassName("registration")[0].style.display = "none";
-       document.getElementsByClassName("aboutMe")[0].style.display = "block";
+       document.getElementById("registrationForm").style.display = "none";
+       document.getElementById("aboutMe").style.display = "block";
    } else {
        if(!emailFlag){
            email.classList.add("error");
@@ -77,7 +80,7 @@ registrationSave.addEventListener('click', () => {
    }
 });
 // Input field login
-let username = document.getElementById('username');
+const username = document.getElementById('username');
 /**
  * If the field is empty, underlines it.
  */
@@ -91,12 +94,12 @@ username.addEventListener('blur', () => {
    }
 });
 // Location field
-let selectLocation = document.getElementById('location');
+const selectLocation = document.getElementById('location');
 /**
  * If the location is not selected, we emphasize the field.
  */
 selectLocation.onchange = () => {
-    let option = selectLocation.querySelectorAll('option')[selectLocation.selectedIndex];
+    const option = selectLocation.querySelectorAll('option')[selectLocation.selectedIndex];
     if(option.getAttribute('value') === ""){
         email.classList.add("error");
         locationFlag = false;
@@ -106,7 +109,7 @@ selectLocation.onchange = () => {
     }
 };
 // Button to save the second form
-let saveAboutMe = document.getElementById('saveAboutMe');
+const saveAboutMe = document.getElementById('saveAboutMe');
 /**
  * All forms are checked for correctness.
  * Those fields that are incorrectly filled - underline.
